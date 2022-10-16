530bcc4d400ed2cf304ee32833f5b16add5fc1fb
dennys-monteiro-daml <dennyshagamenom@gmail.com>
2022-10-16 00:23:35 -0300

    Increment of dynamics data with arrays and obj

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
 import { Box } from '@mui/material';
 import type { NextPage } from 'next'
+import { Teacher } from './src/@types/prof';
 import ListaProf from "./src/components/lista/listaprof";
 
+
 const Home: NextPage = () => {
+  const profs: Teacher [] = [
+    {
+      id: 1,
+      name: "Teacher One",
+      img: "https://github.com/dennys-monteiro-daml.png",
+      description: "Professional, please, describe your abilyties and skills heres, for first impressions.",
+      value: 50,
+    },
+    {
+      id: 2,
+      name: "Teacher Two",
+      img: "https://github.com/dennys-monteiro-daml.png",
+      description: "Professional, please, describe your abilyties and skills heres, for first impressions.",
+      value: 100,
+    },
+    {
+      id: 3,
+      name: "Teacher Three",
+      img: "https://github.com/dennys-monteiro-daml.png",
+      description: "Professional, please, describe your abilyties and skills heres, for first impressions.",
+      value: 150,
+    },
+    {
+      id: 4,
+      name: "Teacher Four",
+      img: "https://github.com/dennys-monteiro-daml.png",
+      description: "Professional, please, describe your abilyties and skills heres, for first impressions.",
+      value: 200,
+    }
+  ]
+
   return (
     <Box sx= {{ backgroundColor: 'secondary.main'}}>
-      <ListaProf></ListaProf>
+      <ListaProf profs = {profs} ></ListaProf>
     </Box>
   )
 }
--- pages/src/components/lista/listaprof.tsx
+++ pages/src/components/lista/listaprof.tsx
@@ -1,50 +1,27 @@
 import { Button } from "@mui/material";
+import { Teacher } from "../../@types/prof";
 import { Description, Image, Info, Items, ListaStyled, Name, Value } from "./listaprof.styled";
 
 
-const ListaProf = () => {
+interface ListaProfs {
+  profs:Teacher[], 
+}
+
+const ListaProf = (props: ListaProfs) => {
   return(
     <ListaStyled>
+      {props.profs.map(prof => 
       <Items>
-        <Image src = "https://github.com/dennys-monteiro-daml.png" ></Image>
+        <Image src = {prof.img} ></Image>
         <Info>
-          <Name>Dennys Monteiro</Name>
-          <Value>$15.600 a year or $1.400 a month, or $40.00 a hour.</Value>
-          <Description></Description>
+          <Name>{prof.name}</Name>
+          <Value>{prof.value}</Value>
+          <Description>{prof.description}</Description>
           <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
           <Button sx= {{ width: '85%' }}>Another Button!</Button>
         </Info>
       </Items>
-      <Items>
-        <Image src = "https://github.com/dennys-monteiro-daml.png" ></Image>
-        <Info>
-          <Name>Dennys Monteiro</Name>
-          <Value>$15.600 a year or $1.400 a month, or $40.00 a hour.</Value>
-          <Description></Description>
-          <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
-          <Button sx= {{ width: '85%' }}>Another Button!</Button>
-        </Info>
-      </Items>
-      <Items>
-        <Image src = "https://github.com/dennys-monteiro-daml.png" ></Image>
-        <Info>
-          <Name>Dennys Monteiro</Name>
-          <Value>$15.600 a year or $1.400 a month, or $40.00 a hour.</Value>
-          <Description></Description>
-          <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
-          <Button sx= {{ width: '85%' }}>Another Button!</Button>
-        </Info>
-      </Items>
-      <Items>
-        <Image src = "https://github.com/dennys-monteiro-daml.png" ></Image>
-        <Info>
-          <Name>Dennys Monteiro</Name>
-          <Value>$15.600 a year or $1.400 a month, or $40.00 a hour.</Value>
-          <Description></Description>
-          <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
-          <Button sx= {{ width: '85%' }}>Another Button!</Button>
-        </Info>
-      </Items>
+      )}  
     </ListaStyled>
   )
 }
