63e599f5060a99adeda93fe013a78e0170b13b1e
dennys-monteiro-daml <dennyshagamenom@gmail.com>
2022-10-16 00:41:39 -0300

    Create a model for ListaProf

--- pages/src/components/lista/listaprof.tsx
+++ pages/src/components/lista/listaprof.tsx
@@ -4,26 +4,32 @@ import { Description, Image, Info, Items, ListaStyled, Name, Value } from "./lis
 
 
 interface ListaProfs {
-  profs:Teacher[], 
+  profs: Teacher[],
 }
 
 const ListaProf = (props: ListaProfs) => {
-  return(
-    <ListaStyled>
-      {props.profs.map(prof => 
-      <Items>
-        <Image src = {prof.img} ></Image>
-        <Info>
-          <Name>{prof.name}</Name>
-          <Value>{prof.value}</Value>
-          <Description>{prof.description}</Description>
-          <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
-          <Button sx= {{ width: '85%' }}>Another Button!</Button>
-        </Info>
-      </Items>
-      )}  
-    </ListaStyled>
-  )
-}
+  return (
+    <div>
+      {props.profs.length > 0 ? (
+            <ListaStyled>
+              {props.profs.map( prof => (
+            <Items>
+              <Image src = {prof.img} ></Image>
+              <Info>
+                <Name>{prof.name}</Name>
+                <Value>{prof.value}</Value>
+                <Description>{prof.description}</Description>
+                <Button sx= {{ width: '85%' }}>Send me a menssage!</Button>
+                <Button sx= {{ width: '85%' }}>Another Button!</Button>
+              </Info>
+            </Items>
+            ))}
+          </ListaStyled>
+      ) : (
+        <p>We can't find what are you looking for!</p>
+      )}
+    </div>
+    )
+  }
 
-export default ListaProf;
+  export default ListaProf;
